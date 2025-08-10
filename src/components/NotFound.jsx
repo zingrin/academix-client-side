f29835d68f023971d0ab3a503f2a3e17bb5bd383
zingrin <jerinloncheu@gmail.com>
2025-08-10 18:27:42 +0600

    error page handle polish

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-center px-4">
-      <h1 className="text-8xl font-extrabold text-error mb-4">404</h1>
+      <h1 className="text-2xl font-extrabold text-error mb-4">(.)</h1>
       <h2 className="text-3xl font-bold mb-2">Page Not Found</h2>
       <p className="text-gray-500 mb-6">
         Sorry, the page you are looking for does not exist or has been moved.
